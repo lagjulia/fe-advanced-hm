e87be38767c9030a2430f35dad1e5cf61a6fc43f
lagjulia <spathiphyllum.simon@gmail.com>
2018-05-24 13:45:47 +0300

    третий модуль, второй апдейт

--- module-3/js/index.js
+++ module-3/js/index.js
@@ -1,46 +1,43 @@
 'use strict'
-
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 let login = prompt('Введите логин');
 
-const heckIfLoginExists = function(logins, login) {
-    for (let item of logins) {
-        if (login === item) {
-          let hasLogin = true;
-          return hasLogin;
-        } else {
-            let hasLogin = false;
-            return hasLogin;
-        }
-    }
+const heckIfLoginExists = function(arr, userInput) {
+    let hasLogin;
+    for (let item of arr) 
+    if (userInput === item) {
+        hasLogin = true;
+        return hasLogin
+    } else {
+        hasLogin = false;
+    }   return hasLogin;
 };
 
-const checkLoginValidity = function(login) {
+const checkLoginValidity = function(userInput) {
     let minLength = 4;
     let maxLength = 16;
-    let lengthValidity; {
-          if (login.length >= minLength && login.length <= maxLength) {
-              lengthValidity = true;
-          } else {
-              lengthValidity = false;
-          }
-      }  return lengthValidity;
+    let lengthValidity; 
+    if (userInput === null) {
+        lengthValidity = false;
+    } else if (login.length >= minLength && login.length <= maxLength) {
+        lengthValidity = true;
+    } else {
+        lengthValidity = false;
+    }  return lengthValidity;
 };
 
-  const addLogin = function(logins, login) { 
-    {
-        if (checkLoginValidity(login) === false) {
-            return alert ('Логин не годится');
-        } else {
-            if (heckIfLoginExists (logins, login) === false) {
-                logins.push(login);
-                alert('Логин успешно добавлен');
-            } else {
-                alert('Такой логин уже используется');
-            }
-        }
+
+const addLogin = function(arr, userInput) {
+    if (checkLoginValidity(userInput) === false) {
+        return alert ('Ошибка! Логин должен быть от 4 до 16 символов');
+    } else if (heckIfLoginExists (arr, userInput) === false) {
+        logins.push(userInput);
+        alert('Логин успешно добавлен');
+    } else {
+        alert('Такой логин уже используется');
     }
 };
+
 
 const z = addLogin(logins, login);
 console.log(z);
